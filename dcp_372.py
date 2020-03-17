8601561d2e661b113c414bcdab41bfa13d9baaf8
Jacob Hammontree <jacobhammontree@Jacobs-MacBook-Pro.local>
2020-03-16 21:23:03 -0400

    finished 3/14

--- dcp_372.py
+++ dcp_372.py
@@ -5,3 +5,9 @@ Write a function that takes a natural number as input and returns the number of 
 
 Constraint: don't use any loops.
 """
+
+
+def countDigitsStr(natNum):
+    return len(str(abs(natNum)))
+
+print(countDigitsStr(235235234624563454563423))
